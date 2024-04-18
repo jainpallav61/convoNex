8d55752b4f1287843531b530ce23ddb8e60a46b4
Pallav Jain <jainpallav61@gmail.com>
2024-04-18 12:54:14 +0530

    Deployment setup

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -18,6 +19,21 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+
+// -----------Deployment-----------------
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname1,"/frontend/build")));
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));;
+  })
+} else{
+  app.get("/",(req,res)=>{
+    res.send("API is running Successfully");
+  })
+}
+// -----------Deployment-----------------
 app.use(notFound);
 app.use(errorHandler);
 
